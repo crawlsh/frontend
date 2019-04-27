9034cbe2a6f7e4c5036bdba3ed67d5015e3819b9
shouc <scf@ieee.org>
2019-04-28 06:53:01 +0800

    fix home page width

--- src/components/coreContainer.vue
+++ src/components/coreContainer.vue
@@ -17,8 +17,9 @@
   }
   .searchContainer {
     text-align: left;
+    width: 780px;
     background-color: #FFF;
-    margin: 220px 7.5%;
+    margin: 230px auto 7.5%;
     padding: 30px 60px;
     border-radius: 20px;
     box-shadow: 0 3px 26px 0 rgba(0,0,0,0.20);
